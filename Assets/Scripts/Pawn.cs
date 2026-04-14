c246f81242304d083f961513563beabc545e2db5
Raestkjoot <gunnarmagnum@gmail.com>
2026-04-14 04:18:42 +0200

    basic movement

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed = 10.0f;
+    
     public void Move(Vector2 direction)
     {
-        Debug.Log("Direction: " + direction);
+        Vector3 moveDirection = new(direction.x, 0.0f, direction.y);
+        transform.Translate(moveDirection * (_moveSpeed * Time.deltaTime));
     }
 }

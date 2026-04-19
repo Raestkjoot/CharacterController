56aec39a09a0eb6fd851b41877053cdf35b50319
Raestkjoot <gunnarmagnum@gmail.com>
2026-04-20 00:22:37 +0200

    Gravity

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class Pawn : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 10.0f;
+    [SerializeField] private float _maxMoveSpeed = 10.0f;
+    [SerializeField] private float _acceleration = 10.0f;
+    [SerializeField] private float _deceleration = 10.0f;
+    [SerializeField] private float _gravity = 10.0f;
     [SerializeField] private float _skinWidth = 0.015f;
     
     // NOTE: This is cached. We can add a MarkDirty function to refresh if the collider changes size.
     private float _radius;
     private float _halfHeight;
     
-    private Vector3 _velocity = Vector3.zero;
     private bool _isGrounded = false;
+    private Vector2 _horizontalVelocity = Vector2.zero;
+    private float _verticalVelocity = 0.0f;
 
     private const uint MaxBounces = 5;
     
@@ -26,11 +30,13 @@ public class Pawn : MonoBehaviour
 
     public void Move(Vector2 direction)
     {
+        Vector2.MoveTowards(_horizontalVelocity, Vector2.zero, _deceleration * Time.deltaTime);
+        Vector3 accelerationLeft =  new(direction.x, 0.0f, direction.y);
+        accelerationLeft *= _acceleration * Time.deltaTime;
+        Vector3 velocityChange = Vector3.zero;
+        
         Vector3 capsulePoint1 = transform.position + transform.up * _halfHeight;
         Vector3 capsulePoint2 = transform.position - transform.up * _halfHeight;
-        Vector3 movementLeft =  new(direction.x, 0.0f, direction.y);
-        movementLeft *= _moveSpeed * Time.deltaTime;
-        Vector3 move = Vector3.zero;
         RaycastHit hit;
         
         for (int i = 0;; ++i)
@@ -44,50 +50,60 @@ public class Pawn : MonoBehaviour
                     capsulePoint1,
                     capsulePoint2,
                     _radius,
-                    movementLeft,
+                    accelerationLeft,
                     out hit,
-                    movementLeft.magnitude,
+                    accelerationLeft.magnitude,
                     -1,
                     QueryTriggerInteraction.Ignore))
             {
-                Vector3 snapToSurface = movementLeft.normalized * (hit.distance - _skinWidth);
+                Vector3 snapToSurface = accelerationLeft.normalized * (hit.distance - _skinWidth);
 
                 if (snapToSurface.magnitude <= _skinWidth)
                 {
                     snapToSurface = Vector3.zero;
                 }
                 
-                move = snapToSurface;
+                velocityChange = snapToSurface;
                 capsulePoint1 += snapToSurface;
                 capsulePoint2 += snapToSurface;
 
-                movementLeft -= snapToSurface;
-                float mag = movementLeft.magnitude;
-                movementLeft = Vector3.ProjectOnPlane(movementLeft, hit.normal).normalized;
-                movementLeft *= mag;
+                accelerationLeft -= snapToSurface;
+                float mag = accelerationLeft.magnitude;
+                accelerationLeft = Vector3.ProjectOnPlane(accelerationLeft, hit.normal).normalized;
+                accelerationLeft *= mag;
             }
             else
             {
-                move += movementLeft;
+                velocityChange += accelerationLeft;
                 break;
             }
         }
 
-        transform.Translate(move);
+        velocityChange = Vector3.ClampMagnitude(velocityChange, _maxMoveSpeed);
 
-        VisualDebug.Instance.DrawSphere(transform.position + Vector3.down * _halfHeight, _radius);
-        VisualDebug.Instance.DrawSphere((transform.position + Vector3.down * _halfHeight) + Vector3.down * 0.2f , _radius);
+        float maxDist = _skinWidth + _halfHeight;
+        if (_verticalVelocity < 0.0f)
+        {
+            maxDist -= _verticalVelocity;
+        }
         
-        if (Physics.SphereCast(transform.position + Vector3.down * _halfHeight, _radius, Vector3.down, out hit, 0.2f,
+        if (Physics.SphereCast(transform.position, _radius, Vector3.down, out hit, maxDist,
                 -1, QueryTriggerInteraction.Ignore))
         {
             _isGrounded = true;
+            _verticalVelocity = 0.0f;
+            velocityChange.y = _skinWidth + _halfHeight - hit.distance;
         }
         else
         {
             _isGrounded = false;
+            _verticalVelocity -= _gravity * Time.deltaTime;
+            velocityChange.y = _verticalVelocity;
         }
         
-        Debug.Log("Grounded = " + _isGrounded);
+        VisualDebug.Instance.DrawSphere(transform.position, _radius, _isGrounded ? Color.green : Color.red);
+        VisualDebug.Instance.DrawSphere(transform.position + Vector3.down * maxDist, _radius, _isGrounded ? Color.green : Color.red);
+        
+        transform.Translate(velocityChange);
     }
 }
